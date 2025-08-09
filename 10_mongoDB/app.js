466d1c70abd2d2a258e7248895373019d4e6fdd6
Krrishbalana <krrishbalana@gmail.com>
2025-08-09 23:45:57 +0530

    dataBase usease basics

--- 10_mongoDB/app.js
+++ 10_mongoDB/app.js
@@ -1,11 +1,44 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+const port = 3000;
+const userModel = require('./models/user')
 
+
+app.set('view engine', 'ejs');
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
  
 
 app.get("/", (req, res)=>{
-    res.send("Hello doston kaise ho? ");
+    res.render('form');
+}) 
+
+app.get("/read", async (req, res)=>{
+    let allusers = await userModel.find();
+    res.render('users', {users: allusers});
+}) 
+
+app.get("/delete/:id", async (req, res)=>{
+    await userModel.findByIdAndDelete(req.params.id);
+    res.redirect("/read");
+
+})
+app.get("/update")
+
+app.post("/create", async (req, res)=>{
+    let {name, email, image} = req.body;
+
+    let createduser = await userModel.create({
+        name: name,
+        email: email,
+        image: image,
+    })
+
+    res.redirect("/read");
 })
 
-app.listen(3000);
+app.listen(port, ()=>{
+    console.log(`running on http://localhost:${port}`);
+});
